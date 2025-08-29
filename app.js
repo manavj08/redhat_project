cb4ac9d5c829903eac598e2f39ac15bbff5209e1
Manav <manavj0817@gmail.com>
2025-08-29 21:18:16 +0530

    Initialize project with app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello World!\n");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!\n'));
+app.get('/mars', (req, res) => res.send('Hello Mars!\n'));
+
+app.listen(8080, () => console.log('App running on port 8080'));
